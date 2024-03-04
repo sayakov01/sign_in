dc991d1f1f6f340b6906ea1e62280055847effae
SAYAKOV <bairsayak2009@gmail.com>
2024-03-04 20:10:34 +0600

    new commit

---
+++ lib/sign_in.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sign_in/sign_up.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: CustomPaint(
+        painter: BackgroundSignIn(),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: [
+                  _getHeard(),
+                  _getButtomRow(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+_getButtomRow(context) {
+  const SizedBox(height: 50);
+  return Expanded(
+    flex: 2,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        const SizedBox(height: 150),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(width: 2, color: Colors.black12),
+            color: Colors.grey[300],
+          ),
+          child:  TextField(
+            cursorColor: Colors.black,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Email',
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(width: 2, color: Colors.black12),
+            color: Colors.grey[300],
+          ),
+          child: const TextField(
+            obscureText: true,
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Password',
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 80,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.grey[800],
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUp()));
+                    },
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.deepPurple),
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forgot Passwords',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.deepPurple),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+_getHeard() {
+  return Expanded(
+    flex: 1,
+    child: Container(
+      alignment: Alignment.bottomLeft,
+      child: const Text(
+        'Welcome\nBack',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 37),
+      ),
+    ),
+  );
+}
+
+class BackgroundSignIn extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.5);
+    blueWave.quadraticBezierTo(sw * 0.5, sh * 0.45, sw * 0.2, 0);
+    blueWave.close();
+    paint.color = Colors.blue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    Path greyWave = Path();
+    greyWave.lineTo(sw, 0);
+    greyWave.lineTo(sw, sh * 0.1);
+    greyWave.cubicTo(
+        sw * 0.95, sh * 0.15, sw * 0.65, sh * 0.15, sw * 0.6, sh * 0.38);
+    greyWave.cubicTo(sw * 0.52, sh * 0.52, 105, sh * 0.50, 0, sh * 0.4);
+    greyWave.close();
+    paint.color = Colors.grey.shade800;
+    canvas.drawPath(greyWave, paint);
+
+    Path yellowWave = Path();
+    yellowWave.lineTo(sw * 0.4, 0);
+    yellowWave.cubicTo(sw * 0.5, 0, sw * 0.20, sh * 0.03, sw * 0.15, sh * 0.14);
+    yellowWave.quadraticBezierTo(sw * 0.12, sh * 0.2, 0, sh * 0.2);
+    yellowWave.close();
+    paint.color = Colors.orange.shade400;
+    canvas.drawPath(yellowWave, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
